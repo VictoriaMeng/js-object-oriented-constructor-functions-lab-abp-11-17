df07360629e545b53325776ae1a8d18537f4ec74
Victoria Meng <nights1ststar@protonmail.com>
2017-11-09 19:52:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function Scooter (year, color, model) {
 }
 
 function Driver (year, age, experience) {
-  this.year = name;
+  this.name = name;
   this.age = age;
   this.experience = experience;
 }
